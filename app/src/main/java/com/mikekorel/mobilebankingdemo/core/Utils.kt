7228ab4c536051efb5c272f50fefcf18f69ffb9a
Michalis Korelidis <kore.mike@gmail.com>
2022-11-02 18:22:44 +0200

    Enable core library desugaring to gain access to date/time utils & other Java 8 APIs without increasing the minSdk version

--- app/src/main/java/com/mikekorel/mobilebankingdemo/core/Utils.kt
+++ app/src/main/java/com/mikekorel/mobilebankingdemo/core/Utils.kt
@@ -1,7 +1,18 @@
 package com.mikekorel.mobilebankingdemo.core
 
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 fun String.capitalizeFirstChar(locale: Locale = Locale.getDefault()) =
     replaceFirstChar { if (it.isLowerCase()) it.titlecase(locale) else it.toString() }
+
+fun getDateString(time: String): String =
+    LocalDate
+        .parse(time.substringBefore('T'))
+        .format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
+
+
 
